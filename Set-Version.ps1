169d50427f541091e969058604e1cadd4ff75cf0
jasperd <jasper-d@users.noreply.github.com>
2017-03-13 23:10:31 +0100

    Diagnostics...

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -14,20 +14,31 @@ function UpdateVersionFile([string] $major, [string] $minor, [string] $patch){
         patch = "$patch"
     }
 
+    Write-Host "Writing version file" -ForegroundColor Magenta
+    Write-Host "New version is  $($major).$($minor).$($patch)" -ForegroundColor Magenta
+
     ConvertTo-Json -InputObject $version | Out-File -FilePath ".\version.json" -Encoding utf8 
 }
 
 
 function SetVersion(){
     $commitHash = HashToInt32($env:APPVEYOR_REPO_COMMIT)
+    Write-Host "Commit hash is $($env:APPVEYOR_REPO_COMMIT), truncated to Int32 as $($commitHash)"  -ForegroundColor Magenta
 
     if ($env:APPVEYOR_REPO_TAG -and $env:APPVEYOR_REPO_TAG_NAME -and $env:APPVEYOR_REPO_TAG_NAME -cmatch '^\s*(?:(?:v\.)|(?:v))?\s*(?<major>\d{1,9})\.(?<minor>\d{1,9})\.(?<patch>\d{1,9})\s*$') {
+        Write-Host "Matching repo tag found"  -ForegroundColor Magenta
+        Write-Host "Repo tag is $($env:APPVEYOR_REPO_TAG)"  -ForegroundColor Magenta
+
         Update-AppveyorBuild -Version "$($matches['major']).$($matches['minor']).$($matches['patch']).$($commitHash)"
         UpdateVersionFile $matches['major'] $matches['minor'] $matches['patch']
 
         Set-AppveyorBuildVariable -Name "DeployArtifacts" -Value "true"
     } else {
         $version = Get-Content -Raw -Path ".\version.json" | ConvertFrom-Json
+
+        Write-Host "Read version file"  -ForegroundColor Magenta 
+        Write-Host "Current version is $($version.major).$($version.minor).$($version.patch).$($commitHash)"  -ForegroundColor Magenta
+
         Update-AppveyorBuild -Version "$($version.major).$($version.minor).$($version.patch).$($commitHash)"
     }
 }
